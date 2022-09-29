93578142eef1c9009bda6f00919033d96b860a35
Grygoryev <nikesushko@gmail.com>
2022-09-29 18:38:58 +0300

    adding store connection with edit form

--- src/components/Users/Item.vue
+++ src/components/Users/Item.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="user-item">
-    <div>
-      <div><strong>User:</strong> {{ userData.name }}</div>
+    <div class="user-item__content">
+      <p><strong>User:</strong> {{ userData.name }}</p>
+      <p><strong>Username:</strong> {{ userData.username }}</p>
+      <p><strong>Email:</strong> {{ userData.email }}</p>
     </div>
     <div class="user-item__btns">
       <MyButton
@@ -43,6 +45,20 @@
     justify-content: space-between;
     align-items: center;
     margin-bottom: 20px;
+
+    &__content {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+
+      p {
+        margin-bottom: 4px;
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+      }
+    }
   }
   button {
     margin-left: 5px;
